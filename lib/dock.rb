996c511523710f626ec3c65778a898551e2eb0ab
William Fischer <william.fischer42@gmail.com>
2018-09-05 14:46:38 -0600

    Added iteration 4 functionality to Dock class

--- lib/dock.rb
+++ lib/dock.rb
@@ -2,18 +2,30 @@ class Dock
   attr_reader :name,
               :max_rental_time,
               :rented,
-              :revenue
+              :revenue,
+              :charges,
+              :rental_data
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @rented = {}
     @revenue = 0
+    @charges = Hash.new(0)
+    @rental_data = []
   end
 
   def rent(boat, renter)
-    if boat.class == Boat && !@rented.keys.include?(boat)
+    if valid_boat?(boat) && valid_renter?(renter)
       @rented[boat] = renter
     end
+  end
+
+  def valid_boat?(boat)
+    boat.class == Boat && !@rented.keys.include?(boat)
+  end
+
+  def valid_renter?(renter)
+    renter.class == Renter
   end
 
   def log_hour
@@ -21,9 +33,35 @@ class Dock
   end
 
   def return(boat)
-    hours = [boat.hours_rented, max_rental_time].min
-    @revenue += hours * boat.price_per_hour
-    boat.return
+    rental_cost = record_revenue(boat)
+    record_rental_data(boat, rental_cost)
     @rented.delete(boat)
+    boat.return
+  end
+
+  def record_revenue(boat)
+    hours = [boat.hours_rented, max_rental_time].min
+    rental_cost = hours * boat.price_per_hour
+    card = @rented[boat].credit_card_number
+    @charges[card] += rental_cost
+    @revenue += rental_cost
+    return rental_cost
+  end
+
+  def record_rental_data(boat, rental_cost)
+    entry = {type: boat.type,
+             hours: boat.hours_rented,
+             revenue: rental_cost,
+             renter: @rented[boat]}
+    @rental_data << entry
+  end
+
+  def total_hours_by_rental_type
+    hours_by_type = Hash.new(0)
+    @rental_data.each do |type_data|
+      type = type_data[:type]
+      hours_by_type[type] += type_data[:hours]
+    end
+    return hours_by_type
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -39,7 +39,7 @@ class DockTest < Minitest::Test
   end
 
   def test_people_etc_cannot_be_rented
-    @dock.rent(@patrick, @eugine)
+    @dock.rent(@patrick, @eugene)
     @dock.rent("This string is not a boat", @patrick)
     expected = {}
     assert_equal expected, @dock.rented
@@ -47,9 +47,25 @@ class DockTest < Minitest::Test
 
   def test_boats_cannot_be_double_booked
     @dock.rent(@sup_1, @patrick)
-    @dock.rent(@sup_1, @eugine)
+    @dock.rent(@sup_1, @eugene)
 
     expected = {@sup_1 => @patrick}
+    assert_equal expected, @dock.rented
+  end
+
+  def test_boats_cannot_be_rented_by_non_people
+    @dock.rent(@sup_1, "This string is not a person")
+    @dock.rent(@sup_1, @canoe)
+
+    expected = {}
+    assert_equal expected, @dock.rented
+  end
+
+  def test_renter_can_rent_multiple_boats_at_a_time
+    @dock.rent(@sup_1, @patrick)
+    @dock.rent(@canoe, @patrick)
+
+    expected = {@sup_1 => @patrick, @canoe => @patrick}
     assert_equal expected, @dock.rented
   end
 
@@ -57,7 +73,7 @@ class DockTest < Minitest::Test
 
     @dock.rent(@kayak_1, @patrick)
     @dock.log_hour
-    @dock.rent(@sup_2, @eugine)
+    @dock.rent(@sup_2, @eugene)
     @dock.log_hour
 
     assert_equal 1, @sup_2.hours_rented
@@ -100,5 +116,46 @@ class DockTest < Minitest::Test
     @dock.return(@kayak_1)
 
     assert_equal 0, @kayak_1.hours_rented
+  end
+
+  def test_it_can_track_charges
+    @dock.rent(@kayak_1, @patrick)
+    @dock.log_hour
+    @dock.rent(@sup_1, @eugene)
+    @dock.log_hour
+    @dock.return(@sup_1)
+    @dock.return(@kayak_1)
+
+    expected = {"4242424242424242" => 40, "1313131313131313" => 15}
+    assert_equal expected, @dock.charges
+  end
+
+  def test_it_records_rental_data
+    @dock.rent(@kayak_1, @patrick)
+    @dock.log_hour
+    @dock.rent(@kayak_2, @eugene)
+    @dock.log_hour
+    @dock.return(@kayak_2)
+    @dock.return(@kayak_1)
+
+    expected = [{:type=>:kayak, :hours=>1, :revenue=>20, :renter=> @eugene},
+              {:type=>:kayak, :hours=>2, :revenue=>40, :renter=> @patrick}]
+
+    assert_equal expected, @dock.rental_data
+  end
+
+  def test_it_can_get_total_hours_by_rental_type
+    @dock.rent(@kayak_1, @patrick)
+    @dock.log_hour
+    @dock.rent(@kayak_2, @eugene)
+    @dock.log_hour
+    @dock.return(@kayak_2)
+    @dock.return(@kayak_1)
+    @dock.rent(@canoe, @patrick)
+    @dock.log_hour
+    @dock.return(@canoe)
+
+    expected = {:kayak => 3, :canoe => 1}
+    assert_equal expected, @dock.total_hours_by_rental_type
   end
 end
